f45c3b152e9f038562ef09cfc41df95ec581507a
ganeshcts <ganeshmuthukumar.g@cognizant.com>
2020-03-19 17:19:46 -0500

    3 assignments done

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,20 +1,24 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
 @RestController
 public class WelcomeController {
 
-    @GetMapping("/")
-    public String sayHello() {
-        return "hello";
+    private String message;
+
+    @Autowired
+    public WelcomeController(@Value("${welcome.message}") final String message) {
+        this.message = message;
     }
 
-    public static void main(String[] args) {
-        SpringApplication.run(WelcomeController.class, args);
+    @GetMapping("/")
+    public String sayHello() {
+        return this.message;
     }
 }
