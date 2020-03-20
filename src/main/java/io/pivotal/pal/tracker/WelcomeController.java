750b728a387eb25ff8a355fb202414bfceb51a03
ganeshcts <ganeshmuthukumar.g@cognizant.com>
2020-03-19 20:22:12 -0500

    pipeline code

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -2,8 +2,6 @@ package io.pivotal.pal.tracker;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
