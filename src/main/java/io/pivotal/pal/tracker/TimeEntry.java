c7ba7a3381b6dea5f76c0ef01399cf2694a95e3a
ganeshcts <ganeshmuthukumar.g@cognizant.com>
2020-03-20 15:25:34 -0500

    timeentry empty classes. java 11 build

---
+++ src/main/java/io/pivotal/pal/tracker/TimeEntry.java
@@ -0,0 +1,73 @@
+package io.pivotal.pal.tracker;
+
+import java.time.LocalDate;
+
+public class TimeEntry {
+
+    private long id;
+    private long projectId;
+    private long userId;
+    private LocalDate date;
+
+    public TimeEntry() {
+
+    }
+
+    public long getProjectId() {
+        return projectId;
+    }
+
+    public TimeEntry(long id, long projectId, long userId, LocalDate date, int hours) {
+        this.id = id;
+        this.projectId = projectId;
+        this.userId = userId;
+        this.date = date;
+        this.hours = hours;
+    }
+
+    public TimeEntry(long projectId, long userId, LocalDate date, int hours) {
+
+        this.projectId = projectId;
+        this.userId = userId;
+        this.date = date;
+        this.hours = hours;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public int getHours() {
+        return hours;
+    }
+
+    public void setHours(int hours) {
+        this.hours = hours;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public void setProjectId(long projectId) {
+        this.projectId = projectId;
+    }
+
+    private int hours;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
---
+++ src/main/java/io/pivotal/pal/tracker/TimeEntryRepository.java
@@ -0,0 +1,17 @@
+package io.pivotal.pal.tracker;
+
+import java.util.List;
+
+public interface TimeEntryRepository {
+
+    public TimeEntry find(long timeEntryId);
+
+
+    public TimeEntry create(TimeEntry timeEntry);
+
+    public TimeEntry update(long eq, TimeEntry timeEntry);
+
+    public void delete(long timeEntryId) ;
+
+    public List<TimeEntry> list();
+}
