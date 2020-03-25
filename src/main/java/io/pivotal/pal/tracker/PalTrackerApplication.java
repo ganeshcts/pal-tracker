790381dd4045f1a5fc1186fef8ad3c9a48ca80c4
ganeshcts <ganeshmuthukumar.g@cognizant.com>
2020-03-25 14:05:41 -0500

    db changes

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -1,10 +1,22 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class PalTrackerApplication {
+
+    @Autowired
+    DataSource dataSource;
+
+    @Bean
+    public TimeEntryRepository timeEntryRepository() {
+        return new JdbcTimeEntryRepository(dataSource);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(PalTrackerApplication.class, args);
